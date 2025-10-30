f7877993d45c30ad1866359741b46a31c8153453
Edwinmwangi-44paid <edumwas10254@gmail.com>
2025-10-30 13:08:20 +0300

    third commit

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ STEP_SIZE = 3
 
 sentences = []
 next_characters = []
-
+"""
 for i in range(0, len(text) - SEQ_LENGTH, STEP_SIZE):
     sentences.append(text[i: i + SEQ_LENGTH])
     next_characters.append(text[i + SEQ_LENGTH])
@@ -36,8 +36,9 @@ for i, sentence in enumerate(sentences):
     for t, char in enumerate(sentence):
         X[i, t, char_to_index[char]] = 1
     y[i, char_to_index[next_characters[i]]] = 1
+"""
 
-model = Sequential()
+""""model = Sequential()
 model.add(LSTM(128, input_shape=(SEQ_LENGTH, len(characters))))
 model.add(Dense(len(characters)))
 model.add(Activation('softmax'))
@@ -46,5 +47,51 @@ model.compile(loss='categorical_crossentropy', optimizer=RMSprop(learning_rate=0
 
 model.fit(X, y, batch_size=256, epochs=4)
 
-model.save("textgenerator_model.keras")
+model.save("textgenerator_model.keras")"""
 
+model = tf.keras.models.load_model("textgenerator_model.keras")
+
+def sample(preds, temperature=1.0):
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds + 1e-10) / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
+
+
+
+def generate_text(length, temperature):
+    start_index = random.randint(0, len(text) - SEQ_LENGTH - 1)
+    generated = ''
+    sentence = text[start_index: start_index + SEQ_LENGTH]
+    generated += sentence
+    for i in range(length):
+        x_pred = np.zeros((1, SEQ_LENGTH, len(characters)))
+        for t, char in enumerate(sentence):
+            x_pred[0, t, char_to_index[char]] = 1.0
+
+        preds = model.predict(x_pred, verbose=0)[0]
+        next_index = sample(preds, temperature)
+        next_char = index_to_char[next_index]
+
+        generated += next_char
+        sentence = sentence[1:] + next_char
+
+    return generated
+
+
+print('-------0.2--------')
+print(generate_text(300, 0.2))
+
+print('-------0.4--------')
+print(generate_text(300, 0.4))
+
+print('-------0.6--------')
+print(generate_text(300, 0.6))
+
+print('-------0.8--------')
+print(generate_text(300, 0.8))
+
+print('-------1--------')
+print(generate_text(300, 1.0))
